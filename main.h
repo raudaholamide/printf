b5aee5eac1365a3d8f3c7f955e1242ca65bbe693
raudaholamide <raudaholamide@gmail.com>
2023-03-28 15:58:05 -0700

    update

--- main.h
+++ main.h
@@ -43,7 +43,7 @@ typedef struct buffer_s
 typedef struct converter_s
 {
 	unsigned char specifier;
-	unsigned int (*func)(va_list, buffer_t *output,
+	unsigned int (*func)(va_list, buffer_t *,\
 			unsigned char, int, int, unsigned char);
 } converter_t;
 
@@ -119,7 +119,7 @@ unsigned char handle_flags(const char *flags, char *index);
 unsigned char handle_length(const char *modifier, char *index);
 int handle_width(va_list args, const char *modifier, char *index);
 int handle_precision(va_list args, const char *modifier, char *index);
-unsigned int (*handle_specifiers(const char *specifier))(va_list, buffer_t *output,
+unsigned int (*handle_specifiers(const char *specifier))(va_list, buffer_t *,\
 		unsigned char, int, int, unsigned char);
 int get_flag(char s, flags_t *f);
 
