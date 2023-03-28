e6a0edb2f35c3a8b45436f8956edf0a67a6dc63e
raudaholamide <123153259+raudaholamide@users.noreply.github.com>
2023-03-28 23:44:46 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,7 +1,3 @@
-/*
- * File: _printf.c
- */
-
 #include "main.h"
 
 void cleanup(va_list args, buffer_t *output);
@@ -25,7 +21,7 @@ void cleanup(va_list args, buffer_t *output)
  * @format: Character string to print - may contain directives.
  * @output: A buffer_t struct containing a buffer.
  * @args: A va_list of arguments.
- *
+ * \param int Description for int.
  * Return: The number of characters stored to output.
  */
 int run_printf(const char *format, va_list args, buffer_t *output)
@@ -33,7 +29,7 @@ int run_printf(const char *format, va_list args, buffer_t *output)
 	int i, wid, prec, ret = 0;
 	char tmp;
 	unsigned char flags, len;
-	unsigned int (*f)(va_list, buffer_t *output,
+	unsigned int (*f)(va_list, buffer_t *,\
 			unsigned char, int, int, unsigned char);
 
 	for (i = 0; *(format + i); i++)
